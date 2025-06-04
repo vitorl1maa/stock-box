526178ba15ff1805a979f96ef6178de6534ce981
Vitor Lima Nascimento <vitornascimento923@gmail.com>
2025-06-03 22:23:40 -0300

    ✨ feat: created environment and users service

---
+++ src/app/core/interfaces/user.ts
@@ -0,0 +1,8 @@
+export interface UserPayload {
+    firstName: string;
+    lastName: string;
+    email: string;
+    cpf: string;
+    phone: string;
+    password: string;
+}
---
+++ src/app/core/services/api/auth/users.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../../../environments/environments';
+import { HttpClient } from '@angular/common/http';
+import { UserPayload } from '../../../interfaces/user';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private apiUrl = `${environment.apiUrl}/users`;
+
+  constructor(private httpClient: HttpClient) { }
+
+  createUser(payload: UserPayload): Observable<any> {
+    return this.httpClient.post(this.apiUrl, payload)
+  }
+}
